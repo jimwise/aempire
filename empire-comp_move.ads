f1927299bbdc6293bfd94e8415e9bad1ea5c21b3
jwise <jwise>
2006-02-22 21:27:10 +0000

    Change term `Lake' to `Land_Locked' throughout, as current code depended (heavily!) on the fact that a square with no water neighbors `was' a lake.
    Also, use a safer setup for ratio selection (don't use an access type unnecessarily).

--- empire-comp_move.ads
+++ empire-comp_move.ads
@@ -13,27 +13,22 @@ private
 -- for exploration.  Later, we add ships of all kinds for control of
 -- the sea.
 
-   Ratio1 : aliased Piece_Value_Array := (ARMY => 60, TRANSPORT => 20, PATROL => 10, others => 0);
+   Ratios : array (Integer range <>) of Piece_Value_Array :=
+     (1 => (ARMY => 60, TRANSPORT => 20, PATROL => 10, others => 0),
+      2 => (ARMY => 90, TRANSPORT => 40, FIGHTER|PATROL|DESTROYER|SUBMARINE => 10, others => 0),
+      3 => (ARMY => 120, TRANSPORT => 60, FIGHTER|PATROL => 20, DESTROYER|SUBMARINE|CARRIER|BATTLESHIP => 10, others => 0),
+      4 => (ARMY => 150, TRANSPORT => 70, FIGHTER|PATROL => 30, DESTROYER|SUBMARINE => 20, CARRIER|BATTLESHIP => 10, others => 0));
+   subtype Ratio_Range is Integer range Ratios'First .. Ratios'Last;
 
-   Ratio2 : aliased Piece_Value_Array := (ARMY => 90, TRANSPORT => 40,
-                              FIGHTER|PATROL|DESTROYER|SUBMARINE => 10, others => 0);
-
-   Ratio3 : aliased Piece_Value_Array := (ARMY => 120, TRANSPORT => 60, FIGHTER|PATROL => 20,
-                              DESTROYER|SUBMARINE|CARRIER|BATTLESHIP => 10, others => 0);
-
-   Ratio4 : aliased Piece_Value_Array := (ARMY => 150, TRANSPORT => 70, FIGHTER|PATROL => 30,
-                              DESTROYER|SUBMARINE => 20, CARRIER|BATTLESHIP => 10);
-
-   Ratio : Piece_Value_P := Ratio1'Access;
-
+   Ratio : Ratio_Range;
 
    procedure Do_Cities;
    procedure Do_Pieces;
    procedure Check_Endgame;
 
-   procedure Comp_Prod (Cityp : in out City_Info_T; Is_Lake : in Boolean);
+   procedure Comp_Prod (Cityp : in out City_Info_T; Is_Land_Locked : in Boolean);
    -- XXX city_count needs to be in/out?  Or is reset before each call?
-   procedure Comp_Set_Needed (Cityp : in out City_Info_T; City_Count : in out Piece_Value_Array; Army_Ok : in Boolean; Is_Lake : in Boolean);
+   procedure Comp_Set_Needed (Cityp : in out City_Info_T; City_Count : in out Piece_Value_Array; Army_Ok : in Boolean; Is_Land_Locked : in Boolean);
    procedure Comp_Set_Prod (Cityp : in out City_Info_T; Ptype : in Piece_Type_T);
    function Need_More (City_Count : in Piece_Value_Array; Prod1 : in Piece_Type_T; Prod2 : in Piece_Type_T) return Piece_Type_T;
    function Overproduced (Cityp : in City_Info_T; City_Count : in Piece_Value_Array) return Boolean;
@@ -54,7 +49,7 @@ private
    procedure Find_Best_Tt (Best : in out Piece_Info_P; Loc : in Location_T);
 
    function Find_Attack (Loc : in Location_T; Obj_List : in Content_Value_Array; Terrain : Acceptable_Terrain_Array) return Location_T;
-   function Lake (Loc : in Location_T) return Boolean;
+   function Land_Locked (Loc : in Location_T) return Boolean;
    function Nearby_Count (Loc : in Location_T) return Integer;
    function Nearby_Load (Obj : in Piece_Info_T; Loc : in Location_T) return Boolean;
 
